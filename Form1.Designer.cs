624a6ea2050b7b191afac1d61e079eade113e43f
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-01 20:03:05 +0300

    Just a template

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ConEmuInside
+{
+    partial class ChildTerminal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.termPanel = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.macroBtn = new System.Windows.Forms.Button();
+            this.printBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.termPanel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(582, 338);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Terminal";
+            // 
+            // termPanel
+            // 
+            this.termPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(43)))), ((int)(((byte)(54)))));
+            this.termPanel.Location = new System.Drawing.Point(6, 19);
+            this.termPanel.Name = "termPanel";
+            this.termPanel.Size = new System.Drawing.Size(570, 313);
+            this.termPanel.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.macroBtn);
+            this.groupBox2.Controls.Add(this.printBtn);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 356);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(582, 48);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controlling box";
+            // 
+            // macroBtn
+            // 
+            this.macroBtn.Location = new System.Drawing.Point(501, 17);
+            this.macroBtn.Name = "macroBtn";
+            this.macroBtn.Size = new System.Drawing.Size(75, 23);
+            this.macroBtn.TabIndex = 2;
+            this.macroBtn.Text = "GuiMacro";
+            this.macroBtn.UseVisualStyleBackColor = true;
+            // 
+            // printBtn
+            // 
+            this.printBtn.Location = new System.Drawing.Point(420, 17);
+            this.printBtn.Name = "printBtn";
+            this.printBtn.Size = new System.Drawing.Size(75, 23);
+            this.printBtn.TabIndex = 1;
+            this.printBtn.Text = "Print";
+            this.printBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(408, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // ChildTerminal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 416);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ChildTerminal";
+            this.Text = "ConEmu Inside";
+            this.Load += new System.EventHandler(this.ChildTerminal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel termPanel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button macroBtn;
+        private System.Windows.Forms.Button printBtn;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConEmuInside
+{
+    public partial class ChildTerminal : Form
+    {
+        public ChildTerminal()
+        {
+            InitializeComponent();
+        }
+
+        private void ChildTerminal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
