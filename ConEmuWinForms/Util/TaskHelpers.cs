9dcddc8c9ada04a36a349368956d9996443ebc47
hypersw <baltic@hypersw.net>
2016-02-21 18:49:19 +0300

    CompletedTask

---
+++ ConEmuWinForms/Util/TaskHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+
+using JetBrains.Annotations;
+
+namespace ConEmu.WinForms.Util
+{
+	public class TaskHelpers
+	{
+		private static Task _completedTask;
+
+		/// <summary>Gets a task that's already been completed successfully.</summary>
+		[NotNull]
+		public static Task CompletedTask
+		{
+			get
+			{
+				Task task = _completedTask;
+				if(task == null)
+				{
+					var src = new TaskCompletionSource<Missing>();
+					src.SetResult(Missing.Value);
+					_completedTask = task = src.Task;
+				}
+				return task;
+			}
+		}
+	}
+}
