82a91f84d800e85033c94ccf0667a91c50635c7a
hypersw <baltic@hypersw.net>
2016-02-23 07:10:10 +0300

    GetInfo Root helper.

---
+++ ConEmuWinForms/GetInfoRoot.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Xml;
+
+using JetBrains.Annotations;
+
+namespace ConEmu.WinForms
+{
+	/// <summary>
+	/// Handles the <c>GetInfo</c> GuiMacro for the <c>Root</c> command.
+	/// </summary>
+	public class GetInfoRoot
+	{
+		private GetInfoRoot([NotNull] string name, int? pid, bool isRunning, int? exitCode)
+		{
+			if(name == null)
+				throw new ArgumentNullException(nameof(name));
+			Name = name;
+			Pid = pid;
+			IsRunning = isRunning;
+			ExitCode = exitCode;
+		}
+
+		/// <summary>
+		/// Exit code of the payload process, if it has exited.
+		/// </summary>
+		public readonly int? ExitCode;
+
+		/// <summary>
+		/// Whether the payload process were running at the moment when the macro were executed.
+		/// </summary>
+		public readonly bool IsRunning;
+
+		/// <summary>
+		/// Name of the process, should always be available whether it's running or not (yet/already).
+		/// </summary>
+		[NotNull]
+		public readonly string Name;
+
+		/// <summary>
+		/// The process ID, available only when the payload process is running.
+		/// </summary>
+		public readonly int? Pid;
+
+		[NotNull]
+		public static Task<GetInfoRoot> QueryAsync([NotNull] ConEmuSession session)
+		{
+			if(session == null)
+				throw new ArgumentNullException(nameof(session));
+
+			return session.BeginGuiMacro("GetInfo").WithParam("Root").ExecuteAsync().ContinueWith(task =>
+			{
+				if(task.Exception != null)
+					throw task.Exception;
+				GuiMacroResult result = task.Result;
+				if(!result.IsSuccessful)
+					throw new InvalidOperationException("The GetInfo-Root call did not succeed.");
+				if(string.IsNullOrWhiteSpace(result.Response)) // Might yield an empty string randomly if not ready yet
+					throw new InvalidOperationException("The GetInfo-Root call has yielded an empty result.");
+
+				// Interpret the string as XML
+				var xmlDoc = new XmlDocument();
+				try
+				{
+					xmlDoc.LoadXml(result.Response);
+				}
+				catch(Exception ex)
+				{
+					// Could not parse the XML response. Not expected. Wait more.
+					throw new InvalidOperationException($"The GetInfo-Root call result “{result.Response}” were not a valid XML document.", ex);
+				}
+				XmlElement xmlRoot = xmlDoc.DocumentElement;
+				if(xmlRoot == null)
+					throw new InvalidOperationException($"The GetInfo-Root call result “{result.Response}” didn't have a root XML element.");
+
+				// Current possible records:
+				// <Root Name="cmd.exe" />
+				// <Root Name="cmd.exe" Running="true" PID="22088" ExitCode="259" UpTime="688343" />
+				// <Root Name="cmd.exe" Running="false" PID="22088" ExitCode="0" UpTime="688343" />
+
+				string sName = xmlRoot.GetAttribute("Name");
+
+				bool isRunningRaw;
+				bool? isRunning = bool.TryParse(xmlRoot.GetAttribute("Running"), out isRunningRaw) ? isRunningRaw : default(bool?); // Might mean the process hasn't started yet, in which case we get an empty attr and can't parse it
+
+				int nPidRaw;
+				int? nPid = ((isRunning == true) && (int.TryParse(xmlRoot.GetAttribute("PID"), NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out nPidRaw))) ? nPidRaw : default(int?);
+
+				int nExitCodeRaw;
+				int? nExitCode = ((isRunning == false) && (int.TryParse(xmlRoot.GetAttribute("ExitCode"), NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out nExitCodeRaw))) ? nExitCodeRaw : default(int?);
+
+				return new GetInfoRoot(sName, nPid, isRunning ?? false, nExitCode);
+			});
+		}
+	}
+}
