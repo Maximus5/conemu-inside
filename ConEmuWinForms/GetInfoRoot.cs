85ff9790fbcc4a6c6cce8e73ceb7351b89da7a9c
hypersw <baltic@hypersw.net>
2016-02-24 13:16:28 +0300

    Use async/await.
    PID is an unsigned int in WinAPI.

--- ConEmuWinForms/GetInfoRoot.cs
+++ ConEmuWinForms/GetInfoRoot.cs
@@ -3,6 +3,8 @@ using System.Globalization;
 using System.Threading.Tasks;
 using System.Xml;
 
+using ConEmu.WinForms.Util;
+
 using JetBrains.Annotations;
 
 namespace ConEmu.WinForms
@@ -12,7 +14,7 @@ namespace ConEmu.WinForms
 	/// </summary>
 	public class GetInfoRoot
 	{
-		private GetInfoRoot([NotNull] string name, int? pid, bool isRunning, int? exitCode)
+		private GetInfoRoot([NotNull] string name, uint? pid, bool isRunning, int? exitCode)
 		{
 			if(name == null)
 				throw new ArgumentNullException(nameof(name));
@@ -41,57 +43,52 @@ namespace ConEmu.WinForms
 		/// <summary>
 		/// The process ID, available only when the payload process is running.
 		/// </summary>
-		public readonly int? Pid;
+		public readonly uint? Pid;
 
 		[NotNull]
-		public static Task<GetInfoRoot> QueryAsync([NotNull] ConEmuSession session)
+		public static async Task<GetInfoRoot> QueryAsync([NotNull] ConEmuSession session)
 		{
 			if(session == null)
 				throw new ArgumentNullException(nameof(session));
 
-			return session.BeginGuiMacro("GetInfo").WithParam("Root").ExecuteAsync().ContinueWith(task =>
+			GuiMacroResult result = await session.BeginGuiMacro("GetInfo").WithParam("Root").ExecuteAsync();
+			if(!result.IsSuccessful)
+				throw new InvalidOperationException("The GetInfo-Root call did not succeed.");
+			if(string.IsNullOrWhiteSpace(result.Response)) // Might yield an empty string randomly if not ready yet
+				throw new InvalidOperationException("The GetInfo-Root call has yielded an empty result.");
+
+			// Interpret the string as XML
+			var xmlDoc = new XmlDocument();
+			try
 			{
-				if(task.Exception != null)
-					throw task.Exception;
-				GuiMacroResult result = task.Result;
-				if(!result.IsSuccessful)
-					throw new InvalidOperationException("The GetInfo-Root call did not succeed.");
-				if(string.IsNullOrWhiteSpace(result.Response)) // Might yield an empty string randomly if not ready yet
-					throw new InvalidOperationException("The GetInfo-Root call has yielded an empty result.");
+				xmlDoc.LoadXml(result.Response);
+			}
+			catch(Exception ex)
+			{
+				// Could not parse the XML response. Not expected. Wait more.
+				throw new InvalidOperationException($"The GetInfo-Root call result “{result.Response}” were not a valid XML document.", ex);
+			}
+			XmlElement xmlRoot = xmlDoc.DocumentElement;
+			if(xmlRoot == null)
+				throw new InvalidOperationException($"The GetInfo-Root call result “{result.Response}” didn't have a root XML element.");
 
-				// Interpret the string as XML
-				var xmlDoc = new XmlDocument();
-				try
-				{
-					xmlDoc.LoadXml(result.Response);
-				}
-				catch(Exception ex)
-				{
-					// Could not parse the XML response. Not expected. Wait more.
-					throw new InvalidOperationException($"The GetInfo-Root call result “{result.Response}” were not a valid XML document.", ex);
-				}
-				XmlElement xmlRoot = xmlDoc.DocumentElement;
-				if(xmlRoot == null)
-					throw new InvalidOperationException($"The GetInfo-Root call result “{result.Response}” didn't have a root XML element.");
+			// Current possible records:
+			// <Root Name="cmd.exe" />
+			// <Root Name="cmd.exe" Running="true" PID="22088" ExitCode="259" UpTime="688343" />
+			// <Root Name="cmd.exe" Running="false" PID="22088" ExitCode="0" UpTime="688343" />
 
-				// Current possible records:
-				// <Root Name="cmd.exe" />
-				// <Root Name="cmd.exe" Running="true" PID="22088" ExitCode="259" UpTime="688343" />
-				// <Root Name="cmd.exe" Running="false" PID="22088" ExitCode="0" UpTime="688343" />
+			string sName = xmlRoot.GetAttribute("Name");
 
-				string sName = xmlRoot.GetAttribute("Name");
+			bool isRunningRaw;
+			bool? isRunning = bool.TryParse(xmlRoot.GetAttribute("Running"), out isRunningRaw) ? isRunningRaw : default(bool?); // Might mean the process hasn't started yet, in which case we get an empty attr and can't parse it
 
-				bool isRunningRaw;
-				bool? isRunning = bool.TryParse(xmlRoot.GetAttribute("Running"), out isRunningRaw) ? isRunningRaw : default(bool?); // Might mean the process hasn't started yet, in which case we get an empty attr and can't parse it
+			uint nPidRaw;
+			uint? nPid = ((isRunning == true) && (uint.TryParse(xmlRoot.GetAttribute("PID"), NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out nPidRaw))) ? nPidRaw : default(uint?);
 
-				int nPidRaw;
-				int? nPid = ((isRunning == true) && (int.TryParse(xmlRoot.GetAttribute("PID"), NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out nPidRaw))) ? nPidRaw : default(int?);
+			int nExitCodeRaw;
+			int? nExitCode = ((isRunning == false) && (int.TryParse(xmlRoot.GetAttribute("ExitCode"), NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out nExitCodeRaw))) ? nExitCodeRaw : default(int?);
 
-				int nExitCodeRaw;
-				int? nExitCode = ((isRunning == false) && (int.TryParse(xmlRoot.GetAttribute("ExitCode"), NumberStyles.Integer, NumberFormatInfo.InvariantInfo, out nExitCodeRaw))) ? nExitCodeRaw : default(int?);
-
-				return new GetInfoRoot(sName, nPid, isRunning ?? false, nExitCode);
-			});
+			return new GetInfoRoot(sName, nPid, isRunning ?? false, nExitCode);
 		}
 	}
 }
