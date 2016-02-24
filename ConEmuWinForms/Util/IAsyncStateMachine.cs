fa8656a604aed38bf9ae966b0ce45f713e89b0f2
hypersw <baltic@hypersw.net>
2016-02-24 13:15:20 +0300

    Stuff to support async/await syntax with netfx4.

---
+++ ConEmuWinForms/Util/IAsyncStateMachine.cs
@@ -0,0 +1,12 @@
+﻿
+#pragma warning disable CheckNamespace
+
+namespace System.Runtime.CompilerServices
+{
+	public interface IAsyncStateMachine
+	{
+		void MoveNext();
+
+		void SetStateMachine(IAsyncStateMachine stateMachine);
+	}
+}
---
+++ ConEmuWinForms/Util/ICriticalNotifyCompletion.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable CheckNamespace
+using System.Security;
+
+namespace System.Runtime.CompilerServices
+{
+	public interface ICriticalNotifyCompletion : INotifyCompletion
+	{
+		[SecurityCritical]
+		void UnsafeOnCompleted(Action continuation);
+	}
+}
