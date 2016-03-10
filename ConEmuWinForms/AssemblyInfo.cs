ab0ee6b6aa0ded1453f71e8986c005ad03df7ad4
hypersw <baltic@hypersw.net>
2016-03-10 03:22:42 +0300

    Assembly info, nuspecs, texts, updating nuget dependencies.

--- ConEmuWinForms/AssemblyInfo.cs
+++ ConEmuWinForms/AssemblyInfo.cs
@@ -1,36 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConEmuWinForms")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConEmuWinForms")]
-[assembly: AssemblyCopyright("Copyright ©hypersw 2015–2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly : AssemblyTitle("ConEmuWinForms")]
+[assembly : AssemblyDescription("This is a console emulator control that embeds a fully functional console view in a Windows Forms window.")]
+[assembly : AssemblyProduct("ConEmu")]
+[assembly : AssemblyCopyright("Copyright © hypersw, Maximus5, 2015–2016")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1dc7d403-484b-43b4-b017-1356397a32cb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// These versions must remain 1.0 as soon as API is considered compatible, this will allow the compiled code to be compatible with minor updates
+// Do not use the nuget version here
+[assembly : AssemblyVersion("1.0.0.0")]
+[assembly : AssemblyFileVersion("1.0.0.0")]
