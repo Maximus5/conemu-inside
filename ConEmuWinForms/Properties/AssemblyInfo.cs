e4662099bc885e85bbfacf1c15263d15a99de021
RussKie <russkie@gmail.com>
2018-11-24 23:50:28 +1100

    Align assembly information across the project

---
+++ ConEmuWinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ConEmuWinForms")]
+[assembly: AssemblyDescription("This is a GitExtensions fork for console emulator control that embeds a fully functional console view in a Windows Forms window.")]
+[assembly: AssemblyCopyright("Copyright © hypersw, Maximus5, 2015–2016")]
