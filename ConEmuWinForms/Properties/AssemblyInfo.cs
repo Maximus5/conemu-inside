37a6a1f7838c1bfbe4f34c550863a8f3ae605ef0
RussKie <russkie@gmail.com>
2018-12-04 23:15:58 +1100

    Fix assembly info

--- ConEmuWinForms/Properties/AssemblyInfo.cs
+++ ConEmuWinForms/Properties/AssemblyInfo.cs
@@ -1,5 +1,30 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ConEmuWinForms")]
 [assembly: AssemblyDescription("This is a GitExtensions fork for console emulator control that embeds a fully functional console view in a Windows Forms window.")]
 [assembly: AssemblyCopyright("Copyright © hypersw, Maximus5, 2015–2016")]
+
+[assembly: AssemblyProduct("GitExtensions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.99.90")]
+[assembly: AssemblyFileVersion("2.99.90")]
+[assembly: AssemblyInformationalVersion("3.00.rc2")]
+
+// Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
+[assembly: CLSCompliant(isCompliant: false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
