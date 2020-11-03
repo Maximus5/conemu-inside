32d483ff7b441d5b881b237a9c49151776c395eb
Maksim Moisiuk <maksim.moisiuk@avira.com>
2020-11-04 00:25:45 +0100

    Update NuGet and build scripts

--- ConEmuWinForms/AssemblyInfo.cs
+++ ConEmuWinForms/AssemblyInfo.cs
@@ -7,5 +7,5 @@
 
 // These versions must remain 1.0 as soon as API is considered compatible, this will allow the compiled code to be compatible with minor updates
 // Do not use the nuget version here
-[assembly : AssemblyVersion("20.10.11.0")]
-[assembly : AssemblyFileVersion("20.10.11.0")]
+[assembly : AssemblyVersion("1.20.10.11")]
+[assembly : AssemblyFileVersion("1.20.10.11")]
--- nuget/build.nuget-org.cmd
+++ nuget/build.nuget-org.cmd
@@ -1,30 +1,14 @@
 @echo off
-cd /d "%~dp0"
+setlocal
 
 rem EXPECTED TO BE CALLED AFTER build.cmd
-rem The version in the package is updated by
-rem build.cmd -> UpdatePackageVersions.ps1
 
-setlocal
-set "PATH=%~dp0..\..\Tools\Nuget\bin;%PATH%"
-rem ??? what would be the path to Nuget.exe cmdline tool?..
+set "PATH=%~dp0..\..\Tools\Nuget\bin;%~dp0..\..\Tools\Arch;%PATH%"
+cd /d "%~dp0..\ConEmuInside\bin\Release"
 
 if exist "ConEmu.Control.WinForms.*.nupkg" del "ConEmu.Control.WinForms.*.nupkg"
-if exist "ConEmu.Control.WinForms\*.nupkg" del "ConEmu.Control.WinForms\*.nupkg"
+if exist "%~dp0ConEmu.Control.WinForms.*.nupkg" del "%~dp0ConEmu.Control.WinForms.*.nupkg"
 
-if not exist data mkdir data
-if not exist data\icon.png (
-  echo Downloading icon
-  ConEmuC -download https://conemu.github.io/img/logo-128.png data\icon.png 2> nul
-  if errorlevel 1 exit /b 1
-)
-if not exist data\license.txt (
-  echo Downloading license
-  ConEmuC -download https://github.com/Maximus5/ConEmu/raw/master/Release/ConEmu/License.txt data\license.txt 2> nul
-  if errorlevel 1 exit /b 1
-)
-
-cd /d ConEmu.Control.WinForms
 call nuget pack -Verbosity detailed -NoDefaultExcludes
 if errorlevel 1 (
   call cecho "Failed to create the package ConEmu.Control.WinForms for Nuget.Org."
@@ -32,7 +16,19 @@ if errorlevel 1 (
 )
 call cecho /green "A package for Nuget.Org were created."
 
-move "%~dp0ConEmu.Control.WinForms\ConEmu.Control.WinForms.*.nupkg" "%~dp0"
+move "%~dp0..\ConEmuInside\bin\Release\ConEmu.Control.WinForms.*.nupkg" "%~dp0"
+if errorlevel 1 (
+  call cecho "Moving ConEmu.Control.WinForms package failed."
+  exit /b 100
+)
+
+set BUILD_NO=20.10.11
+7z a ConEmuInside.%BUILD_NO%.7z *.exe *.dll ConEmu.xml *.config ConEmu\*
+if errorlevel 1 (
+  call cecho "Failed to create the 7zip package."
+  exit /b 100
+)
+move "%~dp0..\ConEmuInside\bin\Release\ConEmuInside.*.7z" "%~dp0"
 if errorlevel 1 (
   call cecho "Moving ConEmu.Control.WinForms package failed."
   exit /b 100
